011d4822a9063de7cf4fa0bc78de8194fb19ed98
André Péninou <andre.peninou@univ-tlse2.fr>
2024-02-28 09:01:36 +0000

    sdsdsd# Please enter the commit message for your changes. Lines starting

--- src/main/java/HelloJava.java
+++ src/main/java/HelloJava.java
@@ -8,6 +8,7 @@ public class     HelloJava {
 	public static void main(String[] args) {
 		System.out.println("Hello Blagnac TEST");
 		System.out.println("Hello Blagnac TEST");
+		System.out.println("Hello Blagnac TEST");
 	}
 
 	public void afficherCancan(){
